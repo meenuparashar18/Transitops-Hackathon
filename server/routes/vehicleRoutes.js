05e3ccc1d1e490851a72fd1fcdf7f6609688687a
Meenu parashar <meenuparashar18@gmail.com>
2026-07-12 16:14:28 +0530

    all setup done for backend

--- server/routes/vehicleRoutes.js
+++ server/routes/vehicleRoutes.js
@@ -9,23 +9,23 @@ import {
   createMaintenanceLog,
   closeMaintenanceLog,
   deleteMaintenanceLog
-} from "../controllers/vechicleController.js";
+} from "../controllers/vechileController.js";
 
 import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// Vehicle Routes
+
 router.get("/", authenticateToken, getVehicles);
 router.post("/", authenticateToken, createVehicle);
 
-// Maintenance Routes
+
 router.get("/maintenance", authenticateToken, getMaintenanceLogs);
 router.post("/maintenance", authenticateToken, createMaintenanceLog);
 router.put("/maintenance/:id/close", authenticateToken, closeMaintenanceLog);
 router.delete("/maintenance/:id", authenticateToken, deleteMaintenanceLog);
 
-// Vehicle by ID Routes
+
 router.get("/:id", authenticateToken, getVehicle);
 router.put("/:id", authenticateToken, updateVehicle);
 router.delete("/:id", authenticateToken, deleteVehicle);
