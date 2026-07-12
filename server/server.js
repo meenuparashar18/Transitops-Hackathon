e8b754cad84a60a565a8f876ccb20987547edb96
Meenu parashar <meenuparashar18@gmail.com>
2026-07-12 11:37:44 +0530

    mongodb conntection done and

--- server/server.js
+++ server/server.js
@@ -1,20 +1,24 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 
+connectDB();
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("TransitOps Server Running 🚛");
+app.get("/", (req,res)=>{
+    res.send("TransitOps API Running 🚛");
 });
+
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT,()=>{
+    console.log(`Server running on ${PORT}`);
 });
